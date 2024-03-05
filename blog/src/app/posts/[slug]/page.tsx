9eb1f1aa0bb4bb90ad0d64b3289b52014f4b0f2b
mihalyi miki <mrmiklosmihalyi@gmail.com>
2024-03-05 08:38:17 +0100

    bug fix

--- blog/src/app/posts/[slug]/page.tsx
+++ blog/src/app/posts/[slug]/page.tsx
@@ -15,7 +15,7 @@ const PostPage = (props: any) => {
         <>
             <div className="flex justify-center items-center v-screen w-full">
                 <div className="bg-opacity-20 backdrop-blur-lg backdrop-filter bg-gray-500 shadow-lg dark:bg-opacity-50 dark:bg-gray-900 rounded-lg p-11 mb-10 min-h-96 mx-10 w-10/12">
-                    <Markdown class="prose prose-invert text-white">{content}</Markdown>
+                    <Markdown class="prose prose-invert text-white break-words">{content}</Markdown>
                 </div>
             </div>
         </>
